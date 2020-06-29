1af6cb72ce01947f41d04bf0703471813e81821e
Sara Kowalski <sarakowalski08@gmail.com>
2020-06-28 23:03:43 -0400

    added score and game restart

---
+++ client/loadingScene.js
@@ -0,0 +1,24 @@
+// import Phaser from 'phaser'
+
+// class LoadingScene extends Phaser.Scene {
+//   constructor() {
+//     super({
+//       key: 'loaidngScene'
+//     })
+//   }
+
+//   preload() {
+//     this.load.image('background', '../public/assets/images/myStackathon.png')
+//     this.load.audio('rick', './public/assests/rick-rolled.oog')
+//   }
+
+//   create() {
+//     let bg = this.add.sprite(0,0, 'background')
+//     bg.setOrigin(0,0)
+//     let opener = this.rickRolled = this.sound.add('rick')
+//   //   this.rickRolled.play()
+//     opener.play()
+//   }
+// }
+
+// export default LoadingScene
